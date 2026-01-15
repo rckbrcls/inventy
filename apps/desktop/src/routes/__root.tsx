4c0dcc9cb46337622787c590ae826ff2ff8ecddf
brcls <petrick_barcelos@hotmail.com>
2026-01-14 23:46:39 -0300

    fix height

--- apps/desktop/src/routes/__root.tsx
+++ apps/desktop/src/routes/__root.tsx
@@ -61,7 +61,7 @@ function RootComponent() {
       <AppSidebar />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center justify-between gap-2 border-b px-4">
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 h-full">
             <SidebarTrigger className="-ml-1 cursor-pointer" />
             <Separator orientation="vertical" className="mr-2 h-full" />
             <Breadcrumb>
